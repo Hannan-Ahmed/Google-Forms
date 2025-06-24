79c3974f5d4c4d1c0d2b4f72dc11ce68094d6510
HANNAN AHMED <108712094+Hannan-Ahmed@users.noreply.github.com>
2025-06-24 20:05:14 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000 // optional if you're already using 3000
+  }
 })
